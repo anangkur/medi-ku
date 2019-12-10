cf923a0201aad88329af9acd90c54e3f478434b6
anangkur <anang.kur@gits.id>
2019-12-10 18:49:22 +0700

    feat(layout login): create layout login activity

--- app/src/main/java/com/anangkur/uangkerja/feature/login/LoginActivity.kt
+++ app/src/main/java/com/anangkur/uangkerja/feature/login/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.anangkur.uangkerja
+package com.anangkur.uangkerja.feature.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anangkur.uangkerja.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
