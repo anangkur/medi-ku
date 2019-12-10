c6059e42ab4c6a3d4fba47c8eaeeaefcdc81a5a4
anangkur <anang.kur@gits.id>
2019-12-10 19:40:14 +0700

    feat(layout login): create layout signup activity

---
+++ app/src/main/java/com/anangkur/uangkerja/feature/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.anangkur.uangkerja.feature.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anangkur.uangkerja.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
