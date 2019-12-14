397325afa554eb403b2e0c9f79f29a4b0f721125
anangkur <anang.kur@gits.id>
2019-12-14 15:58:44 +0700

    feat(layout): create layout home

---
+++ app/src/main/java/com/anangkur/uangkerja/feature/main/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.anangkur.uangkerja.feature.main.home
+
+import androidx.lifecycle.ViewModel
+import com.anangkur.uangkerja.data.Repository
+
+class HomeViewModel(private val repository: Repository): ViewModel(){
+}
