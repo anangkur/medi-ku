b407f14b4b91cd9514429448a308c5718abe7bf1
anangkur <anangk97@gmail.com>
2020-05-18 21:41:56 +0700

    refactor(list-medical-record): move all firebase logic into remote repository.

--- app/src/main/java/com/anangkur/mediku/feature/medicalRecords/listMedicalRecords/MedicalRecordsActivity.kt
+++ app/src/main/java/com/anangkur/mediku/feature/medicalRecords/listMedicalRecords/MedicalRecordsActivity.kt
@@ -2,26 +2,24 @@ package com.anangkur.mediku.feature.medicalRecords.listMedicalRecords
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.anangkur.mediku.R
 import com.anangkur.mediku.base.BaseActivity
-import com.anangkur.mediku.data.model.auth.User
 import com.anangkur.mediku.data.model.medical.MedicalRecord
-import com.anangkur.mediku.feature.medicalRecords.addMedicalRecord.AddMedicalRecordActivity
 import com.anangkur.mediku.feature.covid.covid19.CovidActivity
+import com.anangkur.mediku.feature.medicalRecords.addMedicalRecord.AddMedicalRecordActivity
 import com.anangkur.mediku.feature.medicalRecords.detailMedicalRecord.DetailMedicalRecordActivity
-import com.anangkur.mediku.feature.profile.userProfile.ProfileActivity
-import com.anangkur.mediku.util.*
+import com.anangkur.mediku.util.obtainViewModel
+import com.anangkur.mediku.util.setupRecyclerViewLinear
+import com.anangkur.mediku.util.showSnackbarLong
 import kotlinx.android.synthetic.main.activity_medical_records.*
 import kotlinx.android.synthetic.main.layout_toolbar_back.*
 
 
-class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRecordsActionListener, ForceUpdateChecker.OnUpdateNeededListener {
+class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRecordsActionListener {
 
     companion object{
         fun startActivity(context: Context){
@@ -43,7 +41,6 @@ class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        ForceUpdateChecker.with(this).onUpdateNeeded(this).check()
         setupAdapter()
         observeViewModel()
         swipe_home.setOnRefreshListener {
@@ -56,7 +53,6 @@ class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRe
     override fun onResume() {
         super.onResume()
         mAdapter.resetRecyclerData()
-        mViewModel.getUserProfile()
         mViewModel.getMedicalRecord()
     }
 
@@ -79,20 +75,7 @@ class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRe
             errorGetMedicalRecord.observe(this@MedicalRecordsActivity, Observer {
                 showSnackbarLong(it)
             })
-            progressGetProfile.observe(this@MedicalRecordsActivity, Observer {
-
-            })
-            successGetProfile.observe(this@MedicalRecordsActivity, Observer {
-                
-            })
-            errorGetProfile.observe(this@MedicalRecordsActivity, Observer {
-                showSnackbarLong(it)
-            })
         }
-    }
-
-    override fun onClickProfile() {
-        ProfileActivity.startActivity(this)
     }
 
     override fun onClickAddMedicalRecord() {
@@ -105,21 +88,5 @@ class MedicalRecordsActivity: BaseActivity<MedicalRecordsViewModel>(), MedicalRe
 
     override fun onClickCovid() {
         CovidActivity.startActivity(this)
-    }
-
-    override fun onUpdateNeeded(updateUrl: String?) {
-        val dialog: AlertDialog = AlertDialog.Builder(this)
-            .setTitle("New version available")
-            .setMessage("Please, update app to new version to continue reposting.")
-            .setPositiveButton("Update") { dialog, which -> redirectStore(updateUrl) }
-            .setNegativeButton("No, thanks") { dialog, which -> finish() }
-            .create()
-        dialog.show()
-    }
-
-    private fun redirectStore(updateUrl: String?) {
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(updateUrl))
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/anangkur/mediku/feature/medicalRecords/listMedicalRecords/MedicalRecordsViewModel.kt
+++ app/src/main/java/com/anangkur/mediku/feature/medicalRecords/listMedicalRecords/MedicalRecordsViewModel.kt
@@ -2,6 +2,7 @@ package com.anangkur.mediku.feature.medicalRecords.listMedicalRecords
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.anangkur.mediku.base.BaseFirebaseListener
 import com.anangkur.mediku.data.Repository
 import com.anangkur.mediku.data.model.auth.User
 import com.anangkur.mediku.data.model.medical.MedicalRecord
@@ -19,61 +20,17 @@ class MedicalRecordsViewModel(private val repository: Repository): ViewModel() {
     val errorGetMedicalRecord = MutableLiveData<String>()
     fun getMedicalRecord(){
         CoroutineScope(Dispatchers.IO).launch {
-            try {
-                progressGetMedicalRecord.postValue(true)
-                val user = repository.remoteRepository.firebaseAuth.currentUser
-                repository.remoteRepository.firestore
-                    .collection(Const.COLLECTION_MEDICAL_RECORD)
-                    .document(user?.uid?:"")
-                    .collection(Const.COLLECTION_MEDICAL_RECORD)
-                    .orderBy("createdAt", Query.Direction.DESCENDING)
-                    .get()
-                    .addOnSuccessListener {result ->
-                        progressGetMedicalRecord.postValue(false)
-                        val listData = ArrayList<MedicalRecord>()
-                        for (querySnapshot in result){
-                            val data = querySnapshot.toObject<MedicalRecord>()
-                            if (data != null){
-                                listData.add(data)
-                            }
-                        }
-                        successGetMedicalRecord.postValue(listData)
-                    }
-                    .addOnFailureListener {exception ->
-                        progressGetMedicalRecord.postValue(false)
-                        errorGetMedicalRecord.postValue(exception.message)
-                    }
-            }catch (e: Exception){
-                progressGetMedicalRecord.postValue(false)
-                errorGetMedicalRecord.postValue(e.message)
-            }
-        }
-    }
-
-    val progressGetProfile = MutableLiveData<Boolean>()
-    val successGetProfile = MutableLiveData<User>()
-    val errorGetProfile = MutableLiveData<String>()
-    fun getUserProfile(){
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val user = repository.remoteRepository.firebaseAuth.currentUser
-                progressGetProfile.postValue(true)
-                repository.remoteRepository.firestore
-                    .collection(Const.COLLECTION_USER)
-                    .document(user?.uid?:"")
-                    .get()
-                    .addOnSuccessListener { result ->
-                        progressGetProfile.postValue(false)
-                        successGetProfile.postValue(result.toObject<User>())
-                    }
-                    .addOnFailureListener { exception ->
-                        progressGetProfile.postValue(false)
-                        errorGetProfile.postValue(exception.message)
-                    }
-            }catch (e: Exception){
-                progressGetProfile.postValue(false)
-                errorGetProfile.postValue(e.message)
-            }
+            repository.getMedicalRecords(object: BaseFirebaseListener<List<MedicalRecord>> {
+                override fun onLoading(isLoading: Boolean) {
+                    progressGetMedicalRecord.postValue(isLoading)
+                }
+                override fun onSuccess(data: List<MedicalRecord>) {
+                    successGetMedicalRecord.postValue(data)
+                }
+                override fun onFailed(errorMessage: String) {
+                    errorGetMedicalRecord.postValue(errorMessage)
+                }
+            })
         }
     }
 }
